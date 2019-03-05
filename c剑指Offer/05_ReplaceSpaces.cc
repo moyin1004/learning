cff042bb756d9d743fce4af3c2ffac4e3f4c64f1
moyin1004 <moyin1004@qq.com>
2019-03-05 20:14:09 +0800

    creat 05

---
+++ c剑指Offer/05_ReplaceSpaces.cc
@@ -0,0 +1,41 @@
+/// @file    05_ReplaceSpaces.cc
+/// @author  moyin(moyin1004@163.com)
+/// @data    2019-03-04 20:48:30
+ 
+#include <string.h>
+#include <iostream>
+ 
+using namespace std; 
+
+//思路：从后往前进行插入O(N)
+class Solution {
+public:
+    void replaceSpace(char *str, int length) {
+        if (str == nullptr || length <= 0) return;
+        int spaceNum = 0;
+        int size = 0;
+        while (str[size] != '\0') {
+            if (str[size] == ' ') ++spaceNum;
+            ++size;
+        }
+        int newsize = size + 1 + spaceNum * 2;
+        if (newsize > length) return;
+        for (int i = size; i >= 0; --i) {
+            if (str[i] == ' ') {
+                str[--newsize] = '0';
+                str[--newsize] = '2';
+                str[--newsize] = '%';
+                --i;
+            }
+            str[--newsize] = str[i];
+        }
+    }
+};
+
+int main() {
+    char str[20] = "We are happy";
+    Solution sol;
+    sol.replaceSpace(str, 20);
+    cout << str << endl;
+    return 0;
+}
