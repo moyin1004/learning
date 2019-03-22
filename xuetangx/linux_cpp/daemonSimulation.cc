9823e077dc4eba8c8a91c71a26116c93191c5135
moyin1004 <moyin1004@qq.com>
2019-03-22 14:20:28 +0800

    add comment in 25 line

--- xuetangx/linux_cpp/daemonSimulation.cc
+++ xuetangx/linux_cpp/daemonSimulation.cc
@@ -22,7 +22,7 @@ int main() {
     umask(0); //重设文件掩码
     for (int i = 0; i < 3; ++i) close(i); //关闭文件描述符
     open("/dev/null", O_RDWR); //stdin
-    dup(0);
+    dup(0); //两个文件描述符等位处理
     dup(0);
 
     cout << "daemon work" << endl;
