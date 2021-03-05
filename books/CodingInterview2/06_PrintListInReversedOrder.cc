bf327899cf8c93eeef0b1350f047092e69ca4a98
moyin1004 <moyin1004@qq.com>
2021-03-05 10:52:33 +0800

    mv books

---
+++ books/CodingInterview2/06_PrintListInReversedOrder.cc
@@ -0,0 +1,22 @@
+/// @file    06_PrintListInReversedOrder.cc
+/// @author  moyin(moyin1004@163.com)
+/// @data    2019-03-12 13:23:14
+ 
+#include <iostream>
+#include <vector>
+ 
+using namespace std;
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+    ListNode(int x):
+        var(x), next(NULL) {}
+};
+
+vector<int> printListFromTailToHead(ListNode *head) {
+    
+}
+
+int main() {
+}
