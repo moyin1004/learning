ee2b06d4927eaa811c28ade303ea50f4b6fcedab
moyin1004 <moyin1004@qq.com>
2021-03-12 14:50:34 +0800

    convert tab to space

--- backend-series/net/client.c
+++ backend-series/net/client.c
@@ -12,9 +12,9 @@
 #include <fcntl.h>
 
 
-#define MAX_BUFFER		128
-#define MAX_EPOLLSIZE	(384*1024)
-#define MAX_PORT		100
+#define MAX_BUFFER      128
+#define MAX_EPOLLSIZE   (384*1024)
+#define MAX_PORT        100
 #define MAX_CONN        405000
 
 #define TIME_SUB_MS(tv1, tv2)  ((tv1.tv_sec - tv2.tv_sec) * 1000 + (tv1.tv_usec - tv2.tv_usec) / 1000)
@@ -22,18 +22,18 @@
 int isContinue = 1;
 
 static int ntySetNonblock(int fd) {
-	int flags;
+    int flags;
 
-	flags = fcntl(fd, F_GETFL, 0);
-	if (flags < 0) return flags;
-	flags |= O_NONBLOCK;
-	if (fcntl(fd, F_SETFL, flags) < 0) return -1;
-	return 0;
+    flags = fcntl(fd, F_GETFL, 0);
+    if (flags < 0) return flags;
+    flags |= O_NONBLOCK;
+    if (fcntl(fd, F_SETFL, flags) < 0) return -1;
+    return 0;
 }
 
 static int ntySetReUseAddr(int fd) {
-	int reuse = 1;
-	return setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(reuse));
+    int reuse = 1;
+    return setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(reuse));
 }
 
 static void del_fd(int epoll_fd, int clientfd, int *connections) {
@@ -49,93 +49,93 @@ static void del_fd(int epoll_fd, int clientfd, int *connections) {
 struct epoll_event events[MAX_EPOLLSIZE];
 
 int main(int argc, char **argv) {
-	if (argc <= 2) {
-		printf("Usage: %s ip port\n", argv[0]);
-		exit(0);
-	}
+    if (argc <= 2) {
+        printf("Usage: %s ip port\n", argv[0]);
+        exit(0);
+    }
 
-	const char *ip = argv[1];
-	int port = atoi(argv[2]);
-	int connections = 0;
-	char buffer[128] = {0};
-	int i = 0, index = 0;
+    const char *ip = argv[1];
+    int port = atoi(argv[2]);
+    int connections = 0;
+    char buffer[128] = {0};
+    int i = 0, index = 0;
 
-	int epoll_fd = epoll_create(MAX_EPOLLSIZE);
+    int epoll_fd = epoll_create(MAX_EPOLLSIZE);
 
-	struct sockaddr_in addr;
-	memset(&addr, 0, sizeof(struct sockaddr_in));
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = inet_addr(ip);
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(struct sockaddr_in));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(ip);
 
-	struct timeval tv_begin;
-	gettimeofday(&tv_begin, NULL);
+    struct timeval tv_begin;
+    gettimeofday(&tv_begin, NULL);
 
-	while (1) {
-		if (++index >= MAX_PORT) index = 0;
-		struct epoll_event ev;
-		int sockfd = 0;
+    while (1) {
+        if (++index >= MAX_PORT) index = 0;
+        struct epoll_event ev;
+        int sockfd = 0;
         if (connections == 0 && !isContinue) break;
-		if (connections < MAX_CONN && isContinue) {
-			sockfd = socket(AF_INET, SOCK_STREAM, 0);
-			if (sockfd == -1) {
-				perror("socket");
-				goto err;
-			}
-			addr.sin_port = htons(port+index);
-			if (connect(sockfd, (struct sockaddr*)&addr, sizeof(struct sockaddr_in)) < 0) {
-				perror("connect");
-				goto err;
-			}
-			ntySetNonblock(sockfd);
-			ntySetReUseAddr(sockfd);
+        if (connections < MAX_CONN && isContinue) {
+            sockfd = socket(AF_INET, SOCK_STREAM, 0);
+            if (sockfd == -1) {
+                perror("socket");
+                goto err;
+            }
+            addr.sin_port = htons(port+index);
+            if (connect(sockfd, (struct sockaddr*)&addr, sizeof(struct sockaddr_in)) < 0) {
+                perror("connect");
+                goto err;
+            }
+            ntySetNonblock(sockfd);
+            ntySetReUseAddr(sockfd);
 
-			sprintf(buffer, "Hello Server: client --> %d", connections);
-			send(sockfd, buffer, strlen(buffer), 0);
+            sprintf(buffer, "Hello Server: client --> %d", connections);
+            send(sockfd, buffer, strlen(buffer), 0);
 
-			ev.data.fd = sockfd;
-			ev.events = EPOLLIN | EPOLLOUT;
-			epoll_ctl(epoll_fd, EPOLL_CTL_ADD, sockfd, &ev);
-			connections ++;
-		}
-		if (connections % 1000 == 999 || connections >= MAX_CONN) {
-			struct timeval tv_cur;
-			memcpy(&tv_cur, &tv_begin, sizeof(struct timeval));
-			gettimeofday(&tv_begin, NULL);
-			int time_used = TIME_SUB_MS(tv_begin, tv_cur);
+            ev.data.fd = sockfd;
+            ev.events = EPOLLIN | EPOLLOUT;
+            epoll_ctl(epoll_fd, EPOLL_CTL_ADD, sockfd, &ev);
+            connections ++;
+        }
+        if (connections % 1000 == 999 || connections >= MAX_CONN) {
+            struct timeval tv_cur;
+            memcpy(&tv_cur, &tv_begin, sizeof(struct timeval));
+            gettimeofday(&tv_begin, NULL);
+            int time_used = TIME_SUB_MS(tv_begin, tv_cur);
             if (connections >= MAX_CONN) sleep(1);
-			printf("connections: %d, sockfd:%d, time_used:%d\n", connections, sockfd, time_used);
+            printf("connections: %d, sockfd:%d, time_used:%d\n", connections, sockfd, time_used);
 
-			int nfds = epoll_wait(epoll_fd, events, MAX_EPOLLSIZE, 100);
-			for (i = 0;i < nfds;i ++) {
-				int clientfd = events[i].data.fd;
+            int nfds = epoll_wait(epoll_fd, events, MAX_EPOLLSIZE, 100);
+            for (i = 0;i < nfds;i ++) {
+                int clientfd = events[i].data.fd;
 
-			    if (events[i].events & EPOLLIN) {
-					char rBuffer[MAX_BUFFER] = {0};				
-					ssize_t length = recv(clientfd, rBuffer, MAX_BUFFER, 0);
-					if (length > 0) {
-						printf("RecvBuffer:%s\n", rBuffer);
+                if (events[i].events & EPOLLIN) {
+                    char rBuffer[MAX_BUFFER] = {0};             
+                    ssize_t length = recv(clientfd, rBuffer, MAX_BUFFER, 0);
+                    if (length > 0) {
+                        printf("RecvBuffer:%s\n", rBuffer);
                         if (!strcmp(rBuffer, "quit")) {
-							isContinue = 0;
-						}
-					}
+                            isContinue = 0;
+                        }
+                    }
                     else if (length == 0) {
                         del_fd(epoll_fd, clientfd, &connections);
-					} else {
-						if (errno == EINTR || errno == EAGAIN) continue;
-						printf(" Error clientfd:%d, errno:%d\n", clientfd, errno);
-					}
-				}
+                    } else {
+                        if (errno == EINTR || errno == EAGAIN) continue;
+                        printf(" Error clientfd:%d, errno:%d\n", clientfd, errno);
+                    }
+                }
                 if (!isContinue) {
                     del_fd(epoll_fd, clientfd, &connections);
                 }
-			}
-		}
-		usleep(1 * 100);
-	}
+            }
+        }
+        usleep(1 * 100);
+    }
     close(epoll_fd);
     printf("end\n");
-	return 0;
+    return 0;
 err:
-	printf("error : %s\n", strerror(errno));
-	return 0;
+    printf("error : %s\n", strerror(errno));
+    return 0;
 }
